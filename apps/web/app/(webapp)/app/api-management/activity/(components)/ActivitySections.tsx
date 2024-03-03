b5dc7085d99ec20292c8a48ab19cc631c13ba19f
the-wunmi <adewunmiakinsanya@gmail.com>
2024-03-03 13:02:22 +0100

    fix log.

--- apps/web/app/(webapp)/app/api-management/activity/(components)/ActivitySections.tsx
+++ apps/web/app/(webapp)/app/api-management/activity/(components)/ActivitySections.tsx
@@ -11,103 +11,32 @@ const ActivitySections = ({
   const request = rawData?.request;
   const response = rawData?.response;
 
-const rawCode = path == 'response' ?
-  `{
-    "response": {
-      "headers": {
-        "access-control-allow-origin": "${response?.headers?.['access-control-allow-origin']}",
-        "connection": "${response?.headers?.connection}",
-        "content-type": "${response?.headers?.['content-type']}",
-        "date": "${response?.headers?.date}",
-        "etag": "${response?.headers?.etag}",
-        "request-id": "${response?.headers?.['request-id']}",
-        "transfer-encoding": "${response?.headers?.['transfer-encoding']}",
-        "vary": "${response?.headers?.vary}",
-        "via": "${response?.headers?.via}",
-        "x-kong-proxy-latency": "${response?.headers?.['x-kong-proxy-latency']}",
-        "x-kong-upstream-latency": "${response?.headers?.['x-kong-upstream-latency']}"
-        "x-ratelimit-limit": "${response?.headers?.['x-ratelimit-limit']}"
-        "x-ratelimit-remaining": "${response?.headers?.['x-ratelimit-remaining']}"
-        "x-ratelimit-reset": "${response?.headers?.['x-ratelimit-reset']}"
-        "x-srv-span": "${response?.headers?.['x-srv-span']}"
-        "x-srv-trace": "${response?.headers?.['x-srv-trace']}"
-      },
-      "size": ${response?.size},
-      "status": ${response?.status}
-    }
-  }`
-  :
-  `{
-    "request": {
-      "headers": {
-        "accept": "${request?.headers?.accept}",
-        "accept-encoding": "${request?.headers?.['accept-encoding']}",
-        "cache-control": "${request?.headers?.['cache-control']}",
-        "connection": "${request?.headers?.connection}",
-        "host": "${request?.headers?.host}",
-        "postman-token": "${request?.headers?.['postman-token']}",
-        "request-id": "${request?.headers?.['request-id']}",
-        "user-agent": "${request?.headers?.['user-agent']}",
-        "x-consumer-custom-id": "${request?.headers?.['x-consumer-custom-id']}",
-        "x-consumer-id": "${request?.headers?.['x-consumer-id']}",
-        "x-credential-identifier": "${request?.headers?.['x-credential-identifier']}"
-      },
-      "method": "${request?.method}",
-      "querystring": {},
-      "size": ${request?.size},
-      "uri": "${request?.uri}",
-      "url": "${request?.url}"
-    }
-  }`;
+  const applyStyle = (key: string, value: any, depth = 0): string => {
+    const indentation = ' '.repeat(depth * 2);
 
-const codeElement = path == 'response' ?
-  `{
-    <span style='color: #FB8F8F'>"response"</span>: {
-      <span style='color: #FB8F8F'>"headers"</span>: {
-        <span style='color: #FB8F8F'>"access-control-allow-origin"</span>: <span style='color: #6CE9A6'>"${response?.headers?.['access-control-allow-origin']}"</span>,
-        <span style='color: #FB8F8F'>"connection"</span>: <span style='color: #6CE9A6'>"${response?.headers?.connection}"</span>,
-        <span style='color: #FB8F8F'>"content-type"</span>: <span style='color: #6CE9A6'>"${response?.headers?.['content-type']}"</span>,
-        <span style='color: #FB8F8F'>"date"</span>: <span style='color: #6CE9A6'>"${response?.headers?.date}"</span>,
-        <span style='color: #FB8F8F'>"etag"</span>: <span style='color: #6CE9A6'>"${response?.headers?.etag}"</span>,
-        <span style='color: #FB8F8F'>"request-id"</span>: <span style='color: #6CE9A6'>"${response?.headers?.['request-id']}"</span>,
-        <span style='color: #FB8F8F'>"transfer-encoding"</span>: <span style='color: #6CE9A6'>"${response?.headers?.['transfer-encoding']}"</span>,
-        <span style='color: #FB8F8F'>"vary"</span>: <span style='color: #6CE9A6'>"${response?.headers?.vary}"</span>,
-        <span style='color: #FB8F8F'>"via"</span>: <span style='color: #6CE9A6'>"${response?.headers?.via}"</span>,
-        <span style='color: #FB8F8F'>"x-kong-proxy-latency"</span>: <span style='color: #6CE9A6'>"${response?.headers?.['x-kong-proxy-latency']}"</span>,
-        <span style='color: #FB8F8F'>"x-kong-upstream-latency"</span>: <span style='color: #6CE9A6'>"${response?.headers?.['x-kong-upstream-latency']}"</span>
-        <span style='color: #FB8F8F'>"x-ratelimit-limit"</span>: <span style='color: #6CE9A6'>"${response?.headers?.['x-ratelimit-limit']}"</span>,
-        <span style='color: #FB8F8F'>"x-ratelimit-remaining"</span>: <span style='color: #6CE9A6'>"${response?.headers?.['x-ratelimit-remaining']}"</span>,
-        <span style='color: #FB8F8F'>"x-ratelimit-reset"</span>: <span style='color: #6CE9A6'>"${response?.headers?.['x-ratelimit-reset']}"</span>,
-        <span style='color: #FB8F8F'>"x-srv-span"</span>: <span style='color: #6CE9A6'>"${response?.headers?.['x-srv-span']}"</span>,
-        <span style='color: #FB8F8F'>"x-srv-trace"</span>: <span style='color: #6CE9A6'>"${response?.headers?.['x-srv-trace']}"</span>,
-      },
-      <span style='color: #FB8F8F'>"size"</span>: <span style='color: #6CE9A6'>"${response?.size}"</span>,
-      <span style='color: #FB8F8F'>"status"</span>: <span style='color: #6CE9A6'>"${response?.status}"</span>
+    if (typeof value === 'object' && value !== null) {
+      const styledObject = Object.keys(value).map(innerKey => {
+        const innerValue = value[innerKey];
+        return applyStyle(innerKey, innerValue, depth + 1);
+      });
+      console.log(styledObject)
+      if (!styledObject.length) return `${indentation}<span style='color: #FB8F8F;'>"${key}"</span>: {}`;
+      return `${indentation}<span style='color: #FB8F8F;'>"${key}"</span>: {<br>${styledObject.join(',<br>')}<br>${indentation}}`;
+    } else {
+      return `${indentation}<span style='color: #FB8F8F;'>"${key}"</span>: <span style='color: #6CE9A6;'>${typeof value === "string" ? JSON.stringify(value) : value}</span>`;
     }
-  }`
-  :
-  `{
-    <span style='color: #FB8F8F'>"request"</span>: {
-      <span style='color: #FB8F8F'>"headers"</span>: {
-        <span style='color: #FB8F8F'>"accept"</span>: <span style='color: #6CE9A6'>"${request?.headers?.accept}"</span>,
-        <span style='color: #FB8F8F'>"accept-encoding"</span>: <span style='color: #6CE9A6'>"${request?.headers?.['accept-encoding']}"</span>,
-        <span style='color: #FB8F8F'>"cache-control"</span>: <span style='color: #6CE9A6'>"${request?.headers?.['cache-control']}"</span>,
-        <span style='color: #FB8F8F'>"connection"</span>: <span style='color: #6CE9A6'>"${request?.headers?.connection}"</span>,
-        <span style='color: #FB8F8F'>"host"</span>: <span style='color: #6CE9A6'>"${request?.headers?.host}"</span>,
-        <span style='color: #FB8F8F'>"postman-token"</span>: <span style='color: #6CE9A6'>"${request?.headers?.['postman-token']}"</span>,
-        <span style='color: #FB8F8F'>"request-id"</span>: <span style='color: #6CE9A6'>"${request?.headers?.['request-id']}"</span>,
-        <span style='color: #FB8F8F'>"user-agent"</span>: <span style='color: #6CE9A6'>"${request?.headers?.['user-agent']}"</span>,
-        <span style='color: #FB8F8F'>"x-consumer-custom-id"</span>: <span style='color: #6CE9A6'>"${request?.headers?.['x-consumer-custom-id']}"</span>,
-        <span style='color: #FB8F8F'>"x-consumer-id"</span>: <span style='color: #6CE9A6'>"${request?.headers?.['x-consumer-id']}"</span>,
-        <span style='color: #FB8F8F'>"x-credential-identifier"</span>: <span style='color: #6CE9A6'>"${request?.headers?.['x-credential-identifier']}"</span>
-      },
-      <span style='color: #FB8F8F'>"method"</span>: <span style='color: #6CE9A6'>"${request?.method}"</span>,
-      <span style='color: #FB8F8F'>"querystring"</span>: {},
-      <span style='color: #FB8F8F'>"size"</span>: <span style='color: #6CE9A6'>"${request?.size}"</span>,
-      <span style='color: #FB8F8F'>"uri"</span>: <span style='color: #6CE9A6'>"${request?.uri}"</span>,
-      <span style='color: #FB8F8F'>"url"</span>: <span style='color: #6CE9A6'>"${request?.url}"</span>
-    }
-  }`;
+  };
+
+  const transform = (obj: any) => {
+    const style = Object.keys(obj).map(key => {
+      const value = obj[key];
+      return applyStyle(key, value, 1);
+    });
+    return `{<br>${style.join(',<br>')}${style.length > 0 ? '<br>' : ''}}`;
+  };
+
+  const rawCode = path == 'response' ? JSON.stringify({ response }, undefined, 4): JSON.stringify({ request }, undefined, 4);
+  const codeElement = path == 'response' ? transform({ response }) : transform({ request });
 
   return (
     <section className='w-full h-full flex flex-col gap-[20px]'>
@@ -117,8 +46,8 @@ const codeElement = path == 'response' ?
       />
 
       <div className='w-full flex flex-col h-fit'>
-        <CodeSnippet 
-          rawCode={rawCode} 
+        <CodeSnippet
+          rawCode={rawCode}
           codeElement={codeElement}
         />
       </div>
